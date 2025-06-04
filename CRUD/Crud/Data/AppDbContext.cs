7e392b7572b187c460a4ae68b9795da6b7959b3f
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-06-03 21:43:26 -0400

    creado ProductoDto y AppDbcontext

---
+++ CRUD/Crud/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Crud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ CRUD/Crud/Dtos/ProductoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Crud.Dtos
+{
+    public class ProductoDto
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
