bcba8bc1ee313bba09e93b9a3c00c093c105a5fb
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-06-03 21:42:34 -0400

    creado program

---
+++ CRUD/Crud/Program.cs
@@ -0,0 +1,26 @@
+using Crud.Data;
+using Crud.Repositories;
+using Crud.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ProductoRepository>();
+builder.Services.AddScoped<ProductoService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
